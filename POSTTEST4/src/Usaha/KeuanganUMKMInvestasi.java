bd245b7dfb4ef0674ffa051f429135f56dd47175
mateldayunantaa <145241649+mateldayunantaa@users.noreply.github.com>
2024-04-02 21:25:12 +0800

    Add files via upload

---
+++ POSTTEST4/src/Usaha/KeuanganUMKMInvestasi.java
@@ -0,0 +1,35 @@
+package Usaha;
+
+public class KeuanganUMKMInvestasi extends KeuanganUMKM {
+    private String jenisInvestasi;
+    private double nilaiInvestasi;
+
+    public KeuanganUMKMInvestasi(String idUMKM, int pendapatan, int modalAwal, String sumberModal, String jenisInvestasi, double nilaiInvestasi) {
+        super(idUMKM, pendapatan, modalAwal, sumberModal);
+        this.jenisInvestasi = jenisInvestasi;
+        this.nilaiInvestasi = nilaiInvestasi;
+    }
+
+    public String getJenisInvestasi() {
+        return jenisInvestasi;
+    }
+
+    public void setJenisInvestasi(String jenisInvestasi) {
+        this.jenisInvestasi = jenisInvestasi;
+    }
+
+    public double getNilaiInvestasi() {
+        return nilaiInvestasi;
+    }
+
+    public void setNilaiInvestasi(double nilaiInvestasi) {
+        this.nilaiInvestasi = nilaiInvestasi;
+    }
+    // Overriding
+    @Override
+    public void hitungPendapatan() {
+        double pendapatanBersih = getPendapatan() - getModalAwal();
+        System.out.println("Pendapatan bersih UMKM dengan ID " + getIdUMKM() + " setelah dikurangi modal awal adalah: " + pendapatanBersih);
+    }
+
+}
